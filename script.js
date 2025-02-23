01b1b105cd4ec57abb4e1daf0edfb517c9a13840
pilbazan <71325577+pilbazan@users.noreply.github.com>
2025-02-23 18:21:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,15 +19,6 @@ fetch('datos.json')
       popupAnchor: [1, -34]
     });
 
-    var greenIcon = new L.Icon({
-      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41]
-    });
-
     // Función para mostrar puntos cercanos
     function mostrarPuntosCercanos(lat, lng) {
       // Limpiar marcadores anteriores
@@ -66,8 +57,7 @@ fetch('datos.json')
 
         // Añadir marcador de la ubicación del usuario
         // L.marker([userLat, userLng], { icon: userIcon }).addTo(map)
-        // L.marker([userLat, userLng]).addTo(map)
-           L.marker([userLat, userLng], {icon: greenIcon}).addTo(map);
+         L.marker([userLat, userLng]).addTo(map)
           .bindPopup('¡Estás aquí!')
           .openPopup();
 
